2acf3719d26091c99fe95125292bf68c56342d22
Hisam maulana <hisamcode@gmail.com>
2021-07-24 18:43:31 +0700

    cards - add TestSaveToFileAndNewDeckFromFile

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -9,4 +13,30 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
 
+	// first item slice must Spades of king
+	if d[0] != "Spades of Ace" {
+		t.Errorf("expected Spades of King but get %v", d[0])
+	}
+
+	// last item slice must Clubs of King
+	if d[len(d) - 1] != "Clubs of King" {
+		t.Errorf("Expected Clubs of King but get %v", d[len(d) - 1])
+	}
+
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	// remove file _decktesting
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))	
+	}
+
+	os.Remove("_decktesting") 
 }
